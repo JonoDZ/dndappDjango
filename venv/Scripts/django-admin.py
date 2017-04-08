0a6e11c3701fc17b71663be0edd73c46f7a9e3f7
JonoDZ <jzac90@gmail.com>
2017-04-08 20:55:32 +0100

    removed venv files

--- venv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\jzac9_000\documents\work\casual\dndappdjango\venv\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
